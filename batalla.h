f72be1b928b2085da3a1988134db2ccd9af0a58b
veriiqz <verarojas302@gmail.com>
2025-11-12 16:22:39 -0300

    Add files via upload

---
+++ batalla.h
@@ -0,0 +1,9 @@
+//	batalla.h - prototipo de la función principal de batalla, recibe punteros a pokemon para modificar su nivel
+
+#ifndef BATALLA_H
+#define BATALLA_H
+#include "pokemon.h"
+
+void batalla(Pokemon *aliado, Pokemon *enemigo);
+
+#endif
